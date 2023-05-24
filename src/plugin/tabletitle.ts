44632653f07a62b027c9ace71b5f881bf7aef9dd
7516185Guo <gm990114@163.com>
2023-05-24 19:28:03 +0800

    ts校验

---
+++ src/plugin/tabletitle.ts
@@ -0,0 +1,21 @@
+/**
+ * 平台设备
+ */
+export interface Itltle {
+  /**
+   * 标题
+   */
+  name: string;
+  /**
+   * 宽度
+   */
+  width: string;
+  /**
+   * 代理名称
+   */
+  code: string;
+  /**
+   * 渲染类型
+   */
+  tpl: string;
+}
