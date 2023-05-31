f552232b3529f4c40bbfc0749c082c63123187e4
7516185Guo <gm990114@163.com>
2023-05-31 11:18:29 +0800

    elment plus引入

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -1,29 +1,17 @@
 <template>
   <div class="btn">
-    less
-    {{ user.name }}
     <div class="color fs-xs">我是红色</div>
-    {{ counter.count }}
+    <el-button round>Round</el-button>
+    <el-button type="primary" round>Primary</el-button>
+    <el-button type="success" round>Success</el-button>
+    <el-button type="info" round>Info</el-button>
+    <el-button type="warning" round>Warning</el-button>
+    <el-button type="danger" round>Danger</el-button>
     <router-link to="login">点击去登录</router-link>
   </div>
 </template>
 
 <script setup lang="ts" name="home">
-// import { storeToRefs } from 'pinia'
-// const { count, double } = storeToRefs(counter)
-const { counter, user } = useStore();
-import useStore from "@/store/index.js";
-const api = inject<any>("$api");
-/**
- *  数量
- */
-const num = ref(123);
-console.log(num.value, "这是ref");
-
-onMounted(async () => {
-  const res = await api.getPlatform();
-  console.log(res);
-});
 </script>
 
 <style scoped lang="less">
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import App from "./App.vue";
 import "@/assets/style/common.less";
 import router from "@/router/index.js";
 import Api from "@/api/index.js";
+import ElementPlus from 'element-plus'
+import 'element-plus/theme-chalk/index.css'
 import { createPinia } from "pinia";
 const pinia = createPinia();
 const app = createApp(App);
 app.use(router);
+app.use(ElementPlus);
 app.use(pinia);
 app.provide("$api", Api);
 app.mount("#app");
